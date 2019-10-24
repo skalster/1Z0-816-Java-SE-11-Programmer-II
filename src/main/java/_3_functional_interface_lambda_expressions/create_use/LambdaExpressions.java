bdef48f404cb4c30fd35f3454820a84f58c879f2
Victor Durán <victor.duran@ericsson.com>
2019-10-24 17:05:29 +0200

    First draft of functional interfaces and lambda expressions.

--- src/main/java/_3_functional_interface_lambda_expressions/create_use/LambdaExpressions.java
+++ src/main/java/_3_functional_interface_lambda_expressions/create_use/LambdaExpressions.java
@@ -1,8 +1,30 @@
 package _3_functional_interface_lambda_expressions.create_use;
 
+import java.util.function.BiFunction;
+
 /**
- * including statement lambdas, local-variable for lambda parameters
+ *
+ *
+ * statement lambdas
+ * param -> { return expression; }
+ *
+ * local-variable for lambda parameters
+ *
  */
 public class LambdaExpressions {
+
+    public void localVariableLambdaParameters() {
+
+        BiFunction<String, String, String> concatStrings = (String s1, String s2) -> s1 + s2; // Compiles in Java 8
+
+        BiFunction<String, String, String> concatStrings1 = (var s1, var s2) -> s1 + s2; // Compiles in Java 11, not 10
+
+        BiFunction<String, String, String> concatStrings2 = (s1, s2) -> s1 + s2; // Compiles in Java 11
+
+        BiFunction<String, String, String> concatStrings3 = (@Deprecated var s1, @Deprecated var s2) -> s1 + s2; // Compiles in Java 11
+        // We cannot use such annotations without specifying the types.
+
+
+    }
 
 }
--- src/main/java/_3_functional_interface_lambda_expressions/define_write/FunctionalInterfaceDefinition.java
+++ src/main/java/_3_functional_interface_lambda_expressions/define_write/FunctionalInterfaceDefinition.java
@@ -6,6 +6,9 @@ package _3_functional_interface_lambda_expressions.define_write;
  * It allows a compiler to generate an error if the annotated interface does not satisfy the conditions.
  * Note that Java 8's default methods are not abstract and do not count: a functional interface may still have multiple
  * default methods.
+ *
+ * A  functional  interface  must  have  one  abstract—unimplemented—method.  It may  have  zero  or  more  default
+ * or  implemented  methods.  It  may  also  have  static methods.
  */
 
 @FunctionalInterface
